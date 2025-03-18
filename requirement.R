86868c26e1f1a3eda2e8efe6771cf9c881bcdbf1
hungms <140624966+hungms@users.noreply.github.com>
2025-03-18 23:24:10 +0000

    first commit

---
+++ requirement.R
@@ -0,0 +1,19 @@
+## Boot
+# roxygen2::roxygenise()
+# devtools::document()
+# usethis::use_git()
+# pkgdown::build_site()
+# usethis::use_pkgdown_github_pages()
+# devtools::check()
+# devtools::install()
+# devtools::build()
+# 
+
+
+
+## one time
+# pak::pak("r-lib/httr2")
+# https://stackoverflow.com/questions/72189273/r-cmd-check-github-actions-workflow-failing-on-warnings-notes
+# - uses: r-lib/actions/check-r-package@v1
+#        with:
+#          error-on: '"error"'
